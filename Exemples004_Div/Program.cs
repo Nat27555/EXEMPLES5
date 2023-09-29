945ab40a5ebc9eb95f422dfb53fb7bfc355a8495
Natalia Nosova <nat27555@mail.ru>
2023-09-29 18:08:05 +0300

    Added Exemples004

--- Exemples004_Div/Program.cs
+++ Exemples004_Div/Program.cs
@@ -1,7 +1,18 @@
-﻿//int numberA = 25;
+﻿
+//int numberA = 25;
 //int numberB = 5;
 //Console.WriteLine(numberA / numberB);
 
-double numberA = 57;
-double numberB = 5;
-Console.WriteLine(numberA / numberB);
+//double numberA = 57;
+//double numberB = 5;
+//Console.WriteLine(numberA / numberB);
+int numberA = new Random().Next(1,10); //1 2 3 4 5 6 7 8 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10); //1 2 3 4 5 6 7 8 9
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
+
+
+
+
+

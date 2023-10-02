24eb643f08a963420dfdb0d35450f2d3895070f0
Natalia Nosova <nat27555@mail.ru>
2023-10-02 16:21:15 +0300

    Examples010Array

---
+++ Examples010_Array/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1,2,13,4,25,6,7,8 };
+
+int n = array.Length;
+
+int find = 25;
+int index =0;
+
+while (index<n)
+{
+
+    if (array [index] == find)
+    {
+       Console.WriteLine (index);
+    }
+    index= index+1;
+}

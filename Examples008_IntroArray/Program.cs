12f77f6db06a0f68173b28465587b88449e30292
Natalia Nosova <nat27555@mail.ru>
2023-10-02 16:19:40 +0300

    Examples008IntroArray

---
+++ Examples008_IntroArray/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System;
+int Max(int arg1,int arg2,int arg3)
+{
+    int result = arg1;
+    if(arg2>result) result = arg2;
+    if(arg3>result) result = arg3;
+    return result;
+}
+
+
+
+
+
+
+//            0  1  2  3  4  5  6  7  8       int[] array = {1,23,33,34,35,86,17,18,29};
+//array[0] = 3;
+//Console.WriteLine(array[5]);
+int[]array ={12,23,14,95,66,67,89,17,16};
+int result = Max(
+    Max(array[0],array[1],array[2]),
+    Max(array[3],array[4],array[5]),
+    Max(array[6],array[7],array[8]));
+
+Console.WriteLine(result);
+
+
